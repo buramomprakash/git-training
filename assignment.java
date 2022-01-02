1999d28d82dc62a2d9e3e70432a503709aec01f0
buramomprakash <96856567+buramomprakash@users.noreply.github.com>
2022-01-02 22:44:41 +0530

    assignment1

---
+++ assignment.java
@@ -0,0 +1,26 @@
+package assignment1;
+import java.util.Scanner;
+public class assignment {
+    public static void main(String[] args) {
+        int a,b;
+        Scanner sc1= new Scanner(System.in);
+        System.out.println("enter 1st number");
+        Scanner sc2=new Scanner (System.in);
+        System.out.println("enter 2nd number");
+        a=sc1.nextInt();
+        b=sc2.nextInt();
+        //Addition
+        System.out.println(a+b);
+        //subtraction
+        System.out.println(a-b);
+        //Multiplication
+        System.out.println(a*b);
+        //modulus
+        System.out.println(a%b);
+        
+        
+    
+        }
+    
+    
+}
