04be95f28ade8ea9b7ed026eef8191bfdd1362ba
buramomprakash <96856567+buramomprakash@users.noreply.github.com>
2022-01-06 10:20:41 +0530

    Add files via upload

--- Assignment1.java
+++ Assignment1.java
@@ -1,24 +1,24 @@
-package assignments;
-import java.util.*;
+package Assignment;
 
 public class Assignment1 {
-    public void add(a,b){
+    
+    public void add(int a , int b){
         int sum= a+b;
        System.out.println("additon of two numbers is :" +sum);
     }   
-    public void sub(a,b){
+    public void sub(int a , int b){
         int sub= a-b;
        System.out.println("subtractiton of two numbers is :" +sub);
     }   
-    public void mul(a,b){
+    public void mul(int a , int b){
         int mul= a*b;
        System.out.println("multplication of two numbers is :" +mul);
     }   
-    public void div(a,b){
+    public void div(int a , int b){
         double div= a/b;
        System.out.println("division of two numbers is :" +div);
     }   
-    public void mod(a,b){
+    public void mod(int a , int b){
         int mod= a%b;
        System.out.println("Modulus of two numbers is :" +mod);
     } 
@@ -26,7 +26,7 @@ public class Assignment1 {
     public static void main(String[] args) {
         int a=20 , b=10 ;
         Assignment1 as=new Assignment1();
-        as.add(a,b );
+        as.add(a,b);
         as.sub(a,b);
         as.mul(a,b);
         as.div(a,b);
@@ -34,5 +34,5 @@ public class Assignment1 {
     
     
         
-    }   
-}    
+    }
+}
